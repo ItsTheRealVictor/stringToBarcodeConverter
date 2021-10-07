367781b8b712e3139f5413a70674d1994ceb66e7
ItsTheRealVictor <74051392+ItsTheRealVictor@users.noreply.github.com>
2021-10-07 08:31:33 -0700

    Update barcodeMaker.py
    Added a function to ask the user for the number of strings they want converted into barcodes. Those codes are stored in a folder on the desktop.
    
    Excel sheet functionality is commented out for now, later on I'll add a function which copies the images into an excel sheet stored in the barcodes folder.

--- barcodeMaker.py
+++ barcodeMaker.py
@@ -5,34 +5,32 @@ import openpyxl
 wb = openpyxl.Workbook()
 ws = wb.worksheets[0]
 
-stringOne = 'Hank Hill'
-stringTwo = 'Dale Gribble'
-stringThree = 'John Redcorn'
+numberOfBarcodes = int(input("How many strings do you want to make into barcodes? "))
+stringList = ['PlaceHolder'] * numberOfBarcodes
+for index, string in enumerate(stringList):
+    ask = input(f'Enter string #{index + 1}: ')
+    stringList[index] = ask
 
+def convertToBarcode():
+    for string in stringList:
+        converted = Code128(string, writer=ImageWriter())
+        converted.save(f"C:\\Users\\VD102541\\Desktop\\Barcodes\\{string}")
 
-convertedToBarcode1 = Code128(stringOne, writer=ImageWriter())
-convertedToBarcode1.save(r"C:\Users\VD102541\Desktop\PDX_Archive_Barcode")
+convertToBarcode()
 
-convertedToBarcode2 = Code128(stringTwo, writer=ImageWriter())
-convertedToBarcode2.save(r'C:\Users\VD102541\Desktop\PDX_DeArchive_Barcode')
+# ws['A1'] = stringOne
+# imageOne = openpyxl.drawing.image.Image(r"C:\Users\VD102541\Desktop\PDX_Archive_Barcode.png")
+# imageOne.anchor = 'E1'
+# ws.add_image(imageOne)
 
-convertedToBarcode3 = Code128(stringThree, writer=ImageWriter())
-convertedToBarcode3.save(r"C:\Users\VD102541\Desktop\PDX_ArchiveRecycle_Barcode")
+# ws['A20'] = stringTwo
+# imageTwo = openpyxl.drawing.image.Image(r'C:\Users\VD102541\Desktop\PDX_DeArchive_Barcode.png')
+# imageTwo.anchor = 'E20'
+# ws.add_image(imageTwo)
 
+# ws['A40'] = stringThree
+# imageThree = openpyxl.drawing.image.Image(r"C:\Users\VD102541\Desktop\PDX_ArchiveRecycle_Barcode.png")
+# imageThree.anchor = 'E40'
+# ws.add_image(imageThree)
 
-ws['A1'] = stringOne
-imageOne = openpyxl.drawing.image.Image(r"C:\Users\VD102541\Desktop\PDX_Archive_Barcode.png")
-imageOne.anchor = 'E1'
-ws.add_image(imageOne)
-
-ws['A20'] = stringTwo
-imageTwo = openpyxl.drawing.image.Image(r'C:\Users\VD102541\Desktop\PDX_DeArchive_Barcode.png')
-imageTwo.anchor = 'E20'
-ws.add_image(imageTwo)
-
-ws['A40'] = stringThree
-imageThree = openpyxl.drawing.image.Image(r"C:\Users\VD102541\Desktop\PDX_ArchiveRecycle_Barcode.png")
-imageThree.anchor = 'E40'
-ws.add_image(imageThree)
-
-wb.save(r"C:\Users\VD102541\Desktop\barcodes.xlsx")
+# wb.save(r"C:\Users\VD102541\Desktop\barcodes.xlsx")
